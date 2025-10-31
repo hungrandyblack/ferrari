3d1332e6afe0a13d56d664223d20f17a61b4782b
Hung Randy <lequochung2001@gmail.com>
2025-10-31 11:31:12 +0700

    first commit

---
+++ _next/static/L3ofOlq7pqapYE-nlL3sK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/177-2005d047ee230718.js","static/css/ef46db3751d8e999.css","static/chunks/pages/index-d6b24e1406f4c0df.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
